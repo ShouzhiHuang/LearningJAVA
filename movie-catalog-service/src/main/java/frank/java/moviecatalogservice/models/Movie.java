abb0ad4580f67c8ac399fa7bfdb005190a3f64a9
Frank <frankhuang0730@gmail.com>
2020-08-30 22:53:12 -0400

    catalog details

---
+++ movie-catalog-service/src/main/java/frank/java/moviecatalogservice/models/Movie.java
@@ -0,0 +1,30 @@
+package frank.java.moviecatalogservice.models;
+
+public class Movie {
+
+    private String moiveId;
+    private String name;
+
+    public Movie() { }
+
+    public Movie(String moiveId, String name) {
+        this.moiveId = moiveId;
+        this.name = name;
+    }
+
+    public String getMoiveId() {
+        return moiveId;
+    }
+
+    public void setMoiveId(String moiveId) {
+        this.moiveId = moiveId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
